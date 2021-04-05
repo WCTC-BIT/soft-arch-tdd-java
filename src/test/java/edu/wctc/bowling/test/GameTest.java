92d921696ad5a539e6954f135278d59c8370ba7f
Stacy Read <stacy.read.007@gmail.com>
2021-04-04 20:45:23 -0500

    Can score gutter game

--- src/test/java/edu/wctc/bowling/test/GameTest.java
+++ src/test/java/edu/wctc/bowling/test/GameTest.java
@@ -1,19 +1,38 @@
 package edu.wctc.bowling.test;
 
 import edu.wctc.bowling.Game;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameTest {
+    private Game g;
+
+    @BeforeEach
+    void setUp() {
+        g = new Game();
+    }
+
     @Test
     void canCreateGame() {
-        Game g = new Game();
+//        Game g = new Game();
     }
 
     @Test
     void canRoll()
     {
-        Game g = new Game();
+//        Game g = new Game();
         g.roll(0);
+    }
+
+    @Test
+    void gutterGame() {
+        for (int i = 0; i < 20; i++) {
+            g.roll(0);
+        }
+
+        assertEquals(0, g.score());
     }
 
 }
