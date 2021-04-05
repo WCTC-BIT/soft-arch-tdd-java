213e9ebb25d848fa6a0aa0d4975ab4f9e3771ab6
Stacy Read <stacy.read.007@gmail.com>
2021-04-04 20:47:36 -0500

    Can score all ones

--- src/test/java/edu/wctc/bowling/test/GameTest.java
+++ src/test/java/edu/wctc/bowling/test/GameTest.java
@@ -35,4 +35,13 @@ public class GameTest {
         assertEquals(0, g.score());
     }
 
+    @Test
+    void allOnes() {
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+
+        assertEquals(20, g.score());
+    }
+
 }
